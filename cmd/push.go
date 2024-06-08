ff2490c6a37f139ea14a1379cec70a8b162a8c04
Chad Smith <smith.chad12@gmail.com>
2024-06-08 01:36:36 -0500

    add reading of secrets from user-secrets list

---
+++ cmd/push.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/chadsmith12/dotsec/dotnet"
+	"github.com/spf13/cobra"
+)
+
+// pushCmd represents the push command
+var pushCmd = &cobra.Command{
+	Use:   "push",
+	Short: "Pushes alll the secrets into your file to the secret manager",
+	Long: `Pushes the secrets from the folder specified and saves them to your secret manager folder. There are two types: dotnet or env.
+		dotnet - Uses dotnet user-secrets to set the secrets in your dotnet projects secrets.json file.
+		env - Saves the secrets to the .env file. 
+
+		If you do not specify the --project flag, then it will attempt to use your current working directory.
+		You can specify the project directory for the secrets to try to be read `,
+	Run: pushRun,
+}
+
+func init() {
+	rootCmd.AddCommand(pushCmd)
+	pushCmd.Flags().StringP("project", "p", "", "The path to the dotnet project to sync the secrets to. Default to the current directory. Only valid with --type dotnet.")
+	pushCmd.Flags().StringP("file", "f", ".env", "The env file you want to save the secrets to. Default to .env in the current directory. Only valid with --type env.")
+	pushCmd.Flags().String("type", "dotnet", "The type of secrets file you want to use. dotnet to use dotnet user-secrets or env to use a .env file.")
+}
+
+func pushRun(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		fmt.Println("Specify a folder to download secrets from")
+		os.Exit(1)
+	}
+
+	folderName := args[0]
+	project, _ := cmd.Flags().GetString("project")
+	values, err := dotnet.ListSecrets(project)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error - %v\n", err)
+		os.Exit(1)
+	}
+	for _, value := range values {
+		fmt.Printf(value)
+	}
+	fmt.Printf("Pushing to %s\n", folderName)
+}
--- dotnet/client.go
+++ dotnet/client.go
@@ -1,10 +1,12 @@
 package dotnet
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func InitSecrets(projectPath string) error {
@@ -23,11 +25,46 @@ func SetSecret(projectPath, key, value string) error {
 		cmd.Args = append(cmd.Args, "--project")
 		cmd.Args = append(cmd.Args, projectPath)
 	}
-	
+
 	return logAndRunCommand(cmd)
 }
 
-func logAndRunCommand(cmd *exec.Cmd) error {
+func ListSecrets(projectPath string) ([]string, error) {
+	cmd := exec.Command("dotnet", "user-secrets", "list")
+	if projectPath != "" {
+		cmd.Args = append(cmd.Args, "--project")
+		cmd.Args = append(cmd.Args, projectPath)
+	}
+
+	stdOut, errOut, err := runCmd(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error running %s %s - %v\n", cmd.Args[0], cmd.Args[1], errOut.String())
+		return []string{}, fmt.Errorf("%s %s error: %w", cmd.Args[0], cmd.Args[1], err)
+	}
+
+	scanner := bufio.NewScanner(&stdOut)
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return []string{}, err
+		}
+
+		return []string{}, nil
+	}
+
+	firstline := scanner.Text()
+	if strings.Contains(firstline, "No secrets configured") {
+		return []string{}, nil
+	}
+
+	secrets := []string{firstline}
+	for scanner.Scan() {
+		secrets = append(secrets, scanner.Text())
+	}
+
+	return secrets, nil
+}
+
+func runCmd(cmd *exec.Cmd) (bytes.Buffer, bytes.Buffer, error) {
 	var stdOut bytes.Buffer
 	var errOut bytes.Buffer
 	cmd.Stdout = &stdOut
@@ -35,6 +72,11 @@ func logAndRunCommand(cmd *exec.Cmd) error {
 
 	err := cmd.Run()
 
+	return stdOut, errOut, err
+}
+
+func logAndRunCommand(cmd *exec.Cmd) error {
+	stdOut, errOut, err := runCmd(cmd)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error running %s %s - %v\n", cmd.Args[0], cmd.Args[1], errOut.String())
 		return fmt.Errorf("%s %s error: %w", cmd.Args[0], cmd.Args[1], err)
